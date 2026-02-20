67d97965eeabba45d6d2911754953195fd5ad789
NikT404 <nikhilsinghrajawat0@gmail.com>
2026-02-20 16:11:22 +0530

    load real events

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,13 @@ import EventCard from "./components/EventCard";
 import { IEvent } from "@/database";
 import { cache } from "react";
 import { cacheLife } from "next/cache";
-import events from "@/lib/constants";
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Page =async ()=>{
     'use cache'
     cacheLife('hours')
-    // const response =await fetch(`${BASE_URL}/api/events`);
-    // const { events } = await response.json();
+    const response =await fetch(`${BASE_URL}/api/events`);
+    const { events } = await response.json();
 
     return (
         <section>
@@ -22,13 +21,13 @@ const Page =async ()=>{
             <div className="mt-20 space-y-7">
                 <h3>Featured Events</h3>
 
-                {/* <ul className="list-none events">
+                <ul className="list-none events">
                     { events && events.length>0 && events.map((event:IEvent) => (
                         <li key={event.title} >
                             <EventCard {...event} />
                         </li>
                     ))}
-                </ul> */}
+                </ul>
             </div>
         </section>
     );
